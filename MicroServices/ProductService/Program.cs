c266132909bfbd09001426b4bd918b962a92f947
glenj92 <josh.glen.92@gmail.com>
2025-01-18 19:13:41 -0500

    configuracione de localizacion

--- MicroServices/ProductService/Program.cs
+++ MicroServices/ProductService/Program.cs
@@ -1,3 +1,8 @@
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.EntityFrameworkCore;
+using ProductService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +10,26 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    options.DefaultRequestCulture = new RequestCulture("en-US");
+    options.SupportedCultures = new[] { new CultureInfo("en-US") };
+    options.SupportedUICultures = new[] { new CultureInfo("en-US") };
+});
+
+//Se crea la cadena e conexion que se utilizara.
+//Si se necesitan hacer prueba en el projecto antes de crearlo como Microservicio se necesitara la cadena de conexion en el appsettings.json
+//Aunque para crear el microservicio la cadena de conexion se creara en el Dockerfile y por orden de gerarquia se va a utilizar la cadena que tengas en el archivo Dockerfile antes que la del appsettings.json.
+//Sin embargo donde sea que lo agregues, se debe hacer la configuracion de la cadena de conexion
+//En este caso se pasa la cdena de conexion del Dockerfile que tendra el nombre DefaultConnection
+builder.Services.AddDbContext<ProductContext>(options => 
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//Se agregara el uso de controladores y como es una Web API, no tiene el uso de controladores 
+builder.Services.AddControllers();
+
 var app = builder.Build();
+app.UseRequestLocalization();  
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -16,29 +40,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
+//Si se utiliza autorizacion de coloca 
+app.UseAuthorization();
+//Se agrega el mapeo de controladores
+app.MapControllers();
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
--- MicroServices/ProductService/obj/Debug/net8.0/ProductService.AssemblyInfo.cs
+++ MicroServices/ProductService/obj/Debug/net8.0/ProductService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f00cc6e7f19b322b2a39afc35fc0f6e8c904168")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a5b10b7fe0c2a978d3b654e3e1cd23aed4c3cb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
